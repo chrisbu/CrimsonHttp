2c1a1be00ce88e4e63c2f3a7e337cc707b55d487
Aliaksandr Zhuhrou <zhygrr@gmail.com>
2012-06-11 16:24:33 +0300

    revert back before dart team implement a pub support for windows

--- handlers/HandlersLib.dart
+++ handlers/HandlersLib.dart
@@ -1,6 +1,6 @@
 #library("crimson:handlers");
 #import("../core/CrimsonLib.dart");
-#import("package:log4dart/Lib.dart");
+#import("../../log4dart/Lib.dart");
 #import("dart:crypto");
 #import("dart:io");
 
